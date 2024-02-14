7d3cc1371fb032a6674902db8c1ed28470237b47
aronkesete11@gmail.com <aronkesete11@gmail.com>
2024-02-14 23:38:03 +0100

    hi

--- src/Server/userService/server.js
+++ src/Server/userService/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-const path = require("path");
+//const path = require("path");
 const jwt = require("jsonwebtoken");
 
 // Start the server
